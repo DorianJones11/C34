2fbb1106f9279997f67bbc6e63734a9b5256f58a
DorianJones11 <69775199+DorianJones11@users.noreply.github.com>
2020-10-30 20:53:50 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var ball;
+var database;
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballposition=database.ref('ball/position');
+    ballposition.on("value",readData);
+}
+function readData(data)
+{
+   position = data.val()
+   ball.x = position.xpos;
+   ball.y = position.ypos;
+}
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+database.ref('ball/position').set({
+  xpos:position.xpos+x,
+  ypos:position.ypos+y
+
+});
+   
+}
